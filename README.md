6f769b0a995da129e508288bfe26d655ee752d28
Guillaume Sere <guillaumesere@hotmail.fr>
2025-05-23 14:48:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Application de géolocalisation de restaurants, hôtels, et attractions partout dans le monde :
+
+
+
+![20220820192037](https://user-images.githubusercontent.com/75996200/185759095-8bff2f06-0a25-4210-bd02-cdfb89a9ba1f.png)
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import { CssBaseline } from '@material-ui/core';
+
+const Auth = () => {
+  return (
+    <div>
+    <CssBaseline />
+      <Header /> 
+    </div>
+  )
+}
+
+export default Auth;
